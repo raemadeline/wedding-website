02edb08456708a0121b7aa4ba250e8f411d45681
madeline rae horowitz <raemadeline@gmail.com>
2021-11-23 16:55:28 -0500

    more styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,290 +1,296 @@
 $.fn.isInViewport = function() {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
+	var elementTop = $(this).offset().top;
+	var elementBottom = elementTop + $(this).outerHeight();
 
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+	var viewportTop = $(window).scrollTop();
+	var viewportBottom = viewportTop + $(window).height();
 
-    return elementBottom > viewportTop && elementTop < viewportBottom;
+	return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
+function isDarkMode() {
+	return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
 $(document).ready(function () {
-    // countdown
-    const wedding = new Date('10/23/2022');
-    const today = new Date();
-    const days = Math.ceil((wedding - today) / 86400000);
-    let copy;
-    if (days > 1) {
-        copy = `${days} days to go!`;
-    } else if (days === 1) {
-        copy = 'Tomorrow is the big day!';
-    } else if (days === 0) {
-        copy = 'Today is the big day!';
-    }
+	// countdown
+	const wedding = new Date('10/23/2022');
+	const today = new Date();
+	const days = Math.ceil((wedding - today) / 86400000);
+	let copy;
+	if (days > 1) {
+		copy = `${days} days to go!`;
+	} else if (days === 1) {
+		copy = 'Tomorrow is the big day!';
+	} else if (days === 0) {
+		copy = 'Today is the big day!';
+	}
 
-    if (copy) {
-        document.getElementById('countdown').innerHTML = copy;
-    }
+	if (copy) {
+		document.getElementById('countdown').innerHTML = copy;
+	}
 
-    let originalNavBottom = $('nav').offset().top + $('nav').height();
+	let originalNavBottom = $('nav').offset().top + $('nav').height();
 
-    $(window).resize(() => {
-        originalNavBottom = $('nav').offset().top + $('nav').height();
-    });
+	$(window).resize(() => {
+		originalNavBottom = $('nav').offset().top + $('nav').height();
+	});
 
-    const $navigationLinks = $('nav > ol > li > a');
-    const $sections = $($("section").get().reverse());
-    
-    let sectionIdTonavigationLink = {};
-    $sections.each( function(){
-        sectionIdTonavigationLink[ $(this).attr('id') ] = $('nav > ol > li > a[href=\\#' + $(this).attr('id') + ']');
-    });
+	const $navigationLinks = $('nav > ol > li > a');
+	const $sections = $($("section").get().reverse());
+	
+	let sectionIdTonavigationLink = {};
+	$sections.each( function(){
+		sectionIdTonavigationLink[ $(this).attr('id') ] = $('nav > ol > li > a[href=\\#' + $(this).attr('id') + ']');
+	});
 
-    $(window).scroll(() => {
-        const viewportTop = $(window).scrollTop();
-        const isMobile = $('nav button.mobile').is(':visible')
-        if (viewportTop >= originalNavBottom && !isMobile) {
-            $('nav').addClass('sticky')
-        } else {
-            $('nav').removeClass('sticky')
-        }
+	$(window).scroll(() => {
+		const viewportTop = $(window).scrollTop();
+		const isMobile = $('nav button.mobile').is(':visible')
+		if (viewportTop >= originalNavBottom && !isMobile) {
+			$('nav').addClass('sticky')
+		} else {
+			$('nav').removeClass('sticky')
+		}
 
-        if (!isMobile) {
-            $sections.each(function() {
-                const currentSection = $(this);
-                const sectionTop = currentSection.offset().top;
-        
-                if (viewportTop >= sectionTop - 150) {
-                    const $navigationLink = sectionIdTonavigationLink[currentSection.attr('id')];
-                    if (!$navigationLink.hasClass('active')) {
-                        $navigationLinks.removeClass('active');
-                        $navigationLink.addClass('active');
-                    }
-                    return false;
-                }
-            });
-        }
-    });
+		if (!isMobile) {
+			$sections.each(function() {
+				const currentSection = $(this);
+				const sectionTop = currentSection.offset().top;
+		
+				if (viewportTop >= sectionTop - 150) {
+					const $navigationLink = sectionIdTonavigationLink[currentSection.attr('id')];
+					if (!$navigationLink.hasClass('active')) {
+						$navigationLinks.removeClass('active');
+						$navigationLink.addClass('active');
+					}
+					return false;
+				}
+			});
+		}
+	});
 
-    /********************** RSVP **********************/
-    $('#rsvp-form').on('submit', function (e) {
-        e.preventDefault();
-        var data = $(this).serialize();
+	/********************** RSVP **********************/
+	$('#rsvp-form').on('submit', function (e) {
+		e.preventDefault();
+		var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+		$('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    if (data.result === "error") {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
-                    } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
-                    }
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                });
-        }
-    });
+		if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+			&& MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+			$('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+		} else {
+			$.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+				.done(function (data) {
+					console.log(data);
+					if (data.result === "error") {
+						$('#alert-wrapper').html(alert_markup('danger', data.message));
+					} else {
+						$('#alert-wrapper').html('');
+						$('#rsvp-modal').modal('show');
+					}
+				})
+				.fail(function (data) {
+					console.log(data);
+					$('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+				});
+		}
+	});
 
-    $('nav button.mobile').on('click', function(e) {
-       $('nav').toggleClass('expanded');
-       e.stopPropagation();
-    });
+	$('nav button.mobile').on('click', function(e) {
+	   $('nav').toggleClass('expanded');
+	   e.stopPropagation();
+	});
 
-    $(window).on('click', function() {
-        $('nav').removeClass('expanded');
-    });
+	$(window).on('click', function() {
+		$('nav').removeClass('expanded');
+	});
 });
 
 let fullWidthMap;
 let focusedMap;
 let haveLoadedMarkers = false;
 let correctMapId = '1e81e7338c2717c';
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    correctMapId = 'ed8f84e3fc7b05da';
+if (isDarkMode()) {
+	correctMapId = 'ed8f84e3fc7b05da';
 }
 
 function initMaps() {
-    try {
-        fullWidthMap = new google.maps.Map(document.getElementById("full-width-map"), {
-            center: { lat: 40.662502, lng: -73.96976 },
-            zoom: 14,
-            mapId: correctMapId,
-            disableDoubleClickZoom: true,
-            disableDefaultUI: true,
-            fullscreenControl: false,
-            mapTypeControl: false,
-            zoomControl: false,
-            scaleControl: false,
-            gestureHandling: 'cooperative',
-            isFractionalZoomEnabled: true
-        });
-    
-        focusedMap = new google.maps.Map(document.getElementById("focused-map"), {
-            center: { lat: 40.6877024549926, lng: -73.98270929622757 },
-            zoom: 15.5,
-            mapId: correctMapId,
-            disableDoubleClickZoom: true,
-            disableDefaultUI: true,
-            fullscreenControl: false,
-            mapTypeControl: false,
-            zoomControl: false,
-            scaleControl: false,
-            gestureHandling: 'cooperative',
-            isFractionalZoomEnabled: true
-        });  
-        
-        initMarkers();
-    } catch (error) {
-        console.log(error);
-        $('#focused-map').remove();
-        $('#full-width-map').remove();
-    }
+	try {
+		fullWidthMap = new google.maps.Map(document.getElementById("full-width-map"), {
+			center: { lat: 40.662502, lng: -73.96976 },
+			zoom: 14,
+			mapId: correctMapId,
+			disableDoubleClickZoom: true,
+			disableDefaultUI: true,
+			fullscreenControl: false,
+			mapTypeControl: false,
+			zoomControl: false,
+			scaleControl: false,
+			gestureHandling: 'cooperative',
+			isFractionalZoomEnabled: true
+		});
+	
+		focusedMap = new google.maps.Map(document.getElementById("focused-map"), {
+			center: { lat: 40.6877024549926, lng: -73.98270929622757 },
+			zoom: 15.5,
+			mapId: correctMapId,
+			disableDoubleClickZoom: true,
+			disableDefaultUI: true,
+			fullscreenControl: false,
+			mapTypeControl: false,
+			zoomControl: false,
+			scaleControl: false,
+			gestureHandling: 'cooperative',
+			isFractionalZoomEnabled: true
+		});  
+		
+		initMarkers();
+	} catch (error) {
+		console.log(error);
+		$('#focused-map').remove();
+		$('#full-width-map').remove();
+	}
 }
 
 function initMarkers() {
-  markers.forEach(({name, position: { lat, lng }, type}, index) => {
-    const marker = new mapIcons.Marker({
-        map: focusedMap,
-        title: name,
-        position: new google.maps.LatLng(lat, lng),
-        icon: {
-            path: mapIcons.shapes.MAP_PIN,
-            fillColor: '#6c4040',
-            fillOpacity: 1,
-            strokeColor: '#000000',
-            strokeWeight: 0
-        },
-        map_icon_label: `<span class="map-icon map-icon-${type}"></span>`
-        });
+	const fillColor = isDarkMode() ? '#bfd9d9' : '#6c4040';
+  	markers.forEach(({name, position: { lat, lng }, type}, index) => {
+		const marker = new mapIcons.Marker({
+			map: focusedMap,
+			title: name,
+			position: new google.maps.LatLng(lat, lng),
+			icon: {
+				path: mapIcons.shapes.MAP_PIN,
+				fillColor: fillColor,
+				fillOpacity: 1,
+				strokeColor: '#000000',
+				strokeWeight: 0
+			},
+			map_icon_label: `<span class="map-icon map-icon-${type}"></span>`
+		});
 
-    const infoWindow = new google.maps.InfoWindow({
-        content: name,
-        disableAutoPan: true
-    });
+		const infoWindow = new google.maps.InfoWindow({
+			content: name,
+			disableAutoPan: true
+		});
 
-    const lineElem = $(`#things-to-do a:contains(${name})`);
-    lineElem.on('mouseover', () => {
-        infoWindow.open({map: focusedMap, anchor: marker, shouldFocus: false});
-    });
-    lineElem.on('mouseout', function() {
-        infoWindow.close();
-    });
+		const lineElem = $(`#things-to-do a:contains(${name})`);
+		lineElem.on('mouseover', () => {
+			infoWindow.open({map: focusedMap, anchor: marker, shouldFocus: false});
+		});
+		lineElem.on('mouseout', function() {
+			infoWindow.close();
+		});
 
-    marker.addListener('mouseover', () => {
-        lineElem.addClass('marker-highlight');
-        infoWindow.open({map: focusedMap, anchor: marker, shouldFocus: false});
-    })
-    marker.addListener('mouseout', () => {
-        lineElem.removeClass('marker-highlight');
-        infoWindow.close()
-    })
-  });
+		marker.addListener('mouseover', () => {
+			lineElem.addClass('marker-highlight');
+			infoWindow.open({map: focusedMap, anchor: marker, shouldFocus: false});
+		})
+		marker.addListener('mouseout', () => {
+			lineElem.removeClass('marker-highlight');
+			infoWindow.close()
+		})
+	});
 
-  haveLoadedMarkers = true;
+	haveLoadedMarkers = true;
 }
 
 $('#back-to-top').on('click', () => {
-    $(window).scrollTop(0);
+	$(window).scrollTop(0);
 });
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    correctMapId = e.matches ? "ed8f84e3fc7b05da" : "1e81e7338c2717c";
-    initMaps();
+	correctMapId = e.matches ? "ed8f84e3fc7b05da" : "1e81e7338c2717c";
+	initMaps();
+	initMarkers();
 });
 
 const markers = [
-    {
-        name: 'Sunken Harbor Club',
-        position: { lat: 40.691455273392506, lng: -73.98789318893117 },
-        type: 'night-club'
-    },
-    {
-        name: 'Circa Brewing Co.',
-        position: { lat: 40.69168, lng: -73.986182 },
-        type: 'bar'
-    },
-    {
-        name: 'Alamo Drafthouse',
-        position: { lat: 40.69134307854795, lng: -73.98321997543977 },
-        type: 'movie-theater'
-    },
-    {
-        name: 'Fort Greene Park',
-        position: { lat: 40.691696103920556, lng: -73.97532678072841 },
-        type: 'bicycling'
-    },
-    {
-        name: 'New York Transit Museum',
-        position: { lat: 40.690568558800145, lng: -73.98985656595644 },
-        type: 'train-station'
-    },
-    {
-        name: 'Hilton Brookyln',
-        position: { lat: 40.689587, lng: -73.987997 },
-        type: 'lodging'
-    },
-    {
-        name: 'Coffee Project',
-        position: { lat: 40.68843043640514, lng: -73.97934886309937 },
-        type: 'cafe'
-    },
-    {
-        name: 'Grand Army',
-        position: { lat: 40.68824, lng: -73.986538},
-        type: 'restaurant'
-    },
-    {
-        name: 'Ace Hotel Brookyln',
-        position: { lat: 40.687837, lng: -73.983725 },
-        type: 'lodging'
-    },
-    {
-        name: 'Holiday Inn Brooklyn Downtown',
-        position: { lat: 40.687496, lng: -73.982682 },
-        type: 'lodging'
-    },
-    {
-        name: 'Blue Bottle Coffee',
-        position: { lat: 40.687557477065134, lng: -73.98975447662929},
-        type: 'cafe'
-    },
-    {
-        name: 'BAM',
-        position: { lat: 40.68615201318827, lng: -73.97780054475035 },
-        type: 'museum'
-    },
-    {
-        name: 'Rucola',
-        position: { lat: 40.685611, lng: -73.985892 },
-        type: 'restaurant'
-    },
-    {
-        name: 'Leyenda',
-        position: { lat: 40.68452465535506, lng: -73.99183620294977 },
-        type: 'restaurant'
-    },
-    {
-        name: 'Barely Disfigured',
-        position: { lat: 40.683174371691926, lng: -73.9928010738658},
-        type: 'night-club'
-    },
-    {
-        name: 'Walter\'s',
-        position: {lat: 40.689946647766426, lng: -73.97322191992978},
-        type: 'restaurant'
-    },
-    {
-        name: 'Barclay\'s Center',
-        position: {lat: 40.68283644963635, lng: -73.97577394272429},
-        type: 'stadium'
-    }
+	{
+		name: 'Sunken Harbor Club',
+		position: { lat: 40.691455273392506, lng: -73.98789318893117 },
+		type: 'night-club'
+	},
+	{
+		name: 'Circa Brewing Co.',
+		position: { lat: 40.69168, lng: -73.986182 },
+		type: 'bar'
+	},
+	{
+		name: 'Alamo Drafthouse',
+		position: { lat: 40.69134307854795, lng: -73.98321997543977 },
+		type: 'movie-theater'
+	},
+	{
+		name: 'Fort Greene Park',
+		position: { lat: 40.691696103920556, lng: -73.97532678072841 },
+		type: 'bicycling'
+	},
+	{
+		name: 'New York Transit Museum',
+		position: { lat: 40.690568558800145, lng: -73.98985656595644 },
+		type: 'train-station'
+	},
+	{
+		name: 'Hilton Brookyln',
+		position: { lat: 40.689587, lng: -73.987997 },
+		type: 'lodging'
+	},
+	{
+		name: 'Coffee Project',
+		position: { lat: 40.68843043640514, lng: -73.97934886309937 },
+		type: 'cafe'
+	},
+	{
+		name: 'Grand Army',
+		position: { lat: 40.68824, lng: -73.986538},
+		type: 'restaurant'
+	},
+	{
+		name: 'Ace Hotel Brookyln',
+		position: { lat: 40.687837, lng: -73.983725 },
+		type: 'lodging'
+	},
+	{
+		name: 'Holiday Inn Brooklyn Downtown',
+		position: { lat: 40.687496, lng: -73.982682 },
+		type: 'lodging'
+	},
+	{
+		name: 'Blue Bottle Coffee',
+		position: { lat: 40.687557477065134, lng: -73.98975447662929},
+		type: 'cafe'
+	},
+	{
+		name: 'BAM',
+		position: { lat: 40.68615201318827, lng: -73.97780054475035 },
+		type: 'museum'
+	},
+	{
+		name: 'Rucola',
+		position: { lat: 40.685611, lng: -73.985892 },
+		type: 'restaurant'
+	},
+	{
+		name: 'Leyenda',
+		position: { lat: 40.68452465535506, lng: -73.99183620294977 },
+		type: 'restaurant'
+	},
+	{
+		name: 'Barely Disfigured',
+		position: { lat: 40.683174371691926, lng: -73.9928010738658},
+		type: 'night-club'
+	},
+	{
+		name: 'Walter\'s',
+		position: {lat: 40.689946647766426, lng: -73.97322191992978},
+		type: 'restaurant'
+	},
+	{
+		name: 'Barclay\'s Center',
+		position: {lat: 40.68283644963635, lng: -73.97577394272429},
+		type: 'stadium'
+	}
 ];
